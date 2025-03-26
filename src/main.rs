a4ec7efd33939f39ff39427c7dff40c4644af148
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-26 20:06:44 +0300

    take request headers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use core::str;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+    
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:?}", http_request);
 }

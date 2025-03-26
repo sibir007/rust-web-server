75ca8a420b5c9dde640f1ef0f63c241c4a670646
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-26 22:01:27 +0300

    add ThreadPoll

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,19 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-    
-        thread::spawn(|| {
+        
+        pool.execute(|| {
             handle_connection(stream);
         });
+
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
         // handle_connection(stream);
     }
 }

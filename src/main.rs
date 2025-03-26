0785bdefdb32e31a3d7d54d7eed7bf0910775c81
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-26 21:33:26 +0300

    simulate slow request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::fs;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::Write;
-// use std::io::prelude::*;
-
+// This is a simple web server that responds with a hello.html file when a GET request is made to the root path, and a 404.html file when a GET request is made to any other path.
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    fs,
+    thread,
+    time::Duration,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -22,10 +22,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK\r\n\r\n", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html"),
     };
 
     let response = format!("{}{}", status_line, fs::read_to_string(file_name).unwrap());

6c0db6ea37ed395c930c50473c31af7b5d20344b
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-26 21:13:45 +0300

    write handle for 404

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,30 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        // .map(|mut line| {line.push_str("\n-----------------------------------------------"); line})
-        // .map(|mut line| line.write_str("\n-----------------------------------------------"))
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html").unwrap();
-    let length = content.len();
-    let response =  format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, content);
+    if request_line.starts_with("GET / HTTP/1.1") {
+        respond_with_hello(stream);
+    } else {
+        respond_with_404(stream);
+    }
+}
+
+fn respond_with_hello(mut stream: TcpStream) {
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let body = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("{}{}", response, body);
     stream.write_all(response.as_bytes()).unwrap();
 }
 
+fn respond_with_404(mut stream: TcpStream) {
+    let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    let body = fs::read_to_string("404.html").unwrap();
+
+    let response = format!("{}{}", response, body);
+    stream.write_all(response.as_bytes()).unwrap();
+}
 // fn handle_connection(mut stream: TcpStream) {
 //     let buf_reader = BufReader::new(&stream);
 //     let http_request: Vec<_> = buf_reader

ba00de55ecd4b639bb10a4268c574cf6499f1b46
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-26 19:57:31 +0300

    run server, establish connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use core::str;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
